b79616dd45b549b900e02ae5d1b95744fc09934a
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2017-01-08 20:59:04 -0800

    Add Problem Set 1

---
+++ module_3/ps1.r
@@ -0,0 +1,46 @@
+##############################################################################
+## Problem Set 1: Intro to R
+## Author: Amelia Bertozzi-Villa
+## Description: Most instructions require you to write code. When I ask a 
+##              direct, question please write a brief response in a comment.
+##
+##              Comment your code according to the guidelines discussed in 
+##              class.
+##############################################################################
+
+
+## 1. Basic data types ---------------------------------------------------------
+
+## Divide five by three.
+
+## Have the console print out a greeting.
+
+## Test the truth or falsity of three logical statements.
+
+
+## 2.Variables  ---------------------------------------------------------
+
+## Create a variable with the value of your favorite number. Create another 
+## variable equal to the first variable, squared. Have the console print out 
+## whether or not the second variable is equal to ten. 
+
+## 3. 1-Dimensional Arrays  ----------------------------------------------------
+
+## Create a numeric vector five elements long. 
+## Lengthen it by three elements. Keep only the six elements in the middle. 
+
+## 4. Logic and Loops  ---------------------------------------------------------
+
+## For each element of the vector created in question 3, create a loop that tests
+## if that element is greater than 7.2. If it is, have the console print out your
+## favorite movie quote. If it isn't, print the title of your favorite book.
+
+## Expand the code above such that, if a number is less than 7.2 but greater than 2, 
+## the console prints out the title and artist of the last song you listened to.
+## (Copy and paste your code so I can see both versions.)
+
+
+
+
+
+
