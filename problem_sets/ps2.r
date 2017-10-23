7844280afcfc47d947fdd984fbe54d9ea2b43304
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2017-10-23 11:14:03 +0100

    Update ps2-- more wrangling examples, move merge and reshape to extra credit

--- problem_sets/ps2.r
+++ problem_sets/ps2.r
@@ -2,11 +2,17 @@
 ## Problem Set 2: Data Manipulation
 ## Author: Amelia Bertozzi-Villa
 ## Description: Most instructions require you to write code. When I ask a 
-##              direct, question please write a brief response in a comment.
+##              direct question please write a brief response in a comment.
 ##
 ##              Comment your code according to the guidelines discussed in 
 ##              class.
+##
+##              You can get up to one additional point for the extra credit 
+##              sections.
 ##############################################################################
+# this command *clears your environment* so you start with a fresh slate every time you run the script:
+rm(list=ls()) 
+
 
 ## Section 1: Loading Libraries ---------------------------------------------------------
 
@@ -18,27 +24,58 @@
 
 ## Load the "Suicide" dataset. Read through the documentation for this dataset with "?Suicide"
 
-
-
-## ------------------------------------------------------------------------------------
-## Section 3: data.table commands  ----------------------------------------------------
-
 ## Convert the "Suicide" dataset into a data.table. Remember to *assign* it to an object named "Suicide"!
 
+## What does one line of this dataset represent? Look back at the "What is Data Science" lecture if you need a hint.
+## NOTE: please type your response IN A COMMENT so it doesn't get executed as code. To comment out a block of text, 
+## Select it and hit Cmd-Shift-C. 
+
+## ------------------------------------------------------------------------------------
+## Section 3: data.table : i  ----------------------------------------------------
+## Note: NONE OF THESE COMMANDS should change the original dataset "Suicide". You're just
+## printing subsets of the dataset to the screen.
+
+## Isolate the 7th row of the dataset.
+
+## Isolate rows 3-15 of the dataset. 
+
+## Isolate all the rows where method is equal to "poison". 
+
+## Isolate all the rows where sex is equal to "male" and method is equal to "drown". 
+
+## ------------------------------------------------------------------------------------
+## Section 4: data.table : j  ----------------------------------------------------
+
+## Isolate the column "sex".
+
+## Isolate the columns "Freq" and "age".
+
+## Isolate rows 200-215 AND columns "sex" and "method". 
+
 ## Delete the "method2" and "age.group" columns from your dataset. 
+## NOTE: This WILL change the dataset.
 
-## Isolate all the rows where method is equal to "poison".
+## ------------------------------------------------------------------------------------
+## Section 5: data.table : by  ----------------------------------------------------
 
-## Isolate all the rows where sex is equal to "male" and method is equal to "drown".
+## Aggregate the dataset down to the total number (e.g. the "sum") of suicides, BY method. Do *not* assign this output to an object name. 
 
-## Aggregate the dataset down to the total number (e.g. the "sum") of suicides, by method. (note: do NOT save this to a new dataset)
-
-## Create a *new column* called "mean.freq", equal to the mean frequency of suicide, by method and sex.
+## Create a *new column* called "mean.freq", equal to the mean frequency of suicide, BY method and sex.
+## By "create" here, I mean that you should *update* the dataset such that when you print out "Suicide", you see the new column name.
 
 ## Delete the column "mean.freq".
 
+
 ## ------------------------------------------------------------------------------------
-## Section 4: Merging  ---------------------------------------------------------
+## Section 6: Contemplation -----------------------------------------------------
+## You've just been working with a dataset of suicides. We've been treating them like they're just 
+## any old numbers. Is this an appropriate method of working with this dataset? How do you, as a newfangled 
+## data scientist, feel about needing to work with depressing or sensitive information? 
+## Again, please write your response as a comment. 
+
+
+## ------------------------------------------------------------------------------------
+## EXTRA CREDIT 1: Merging  ---------------------------------------------------------
 
 ## Run the code below to generate a new dataset with the frequency of suicides in neighboring East Germany:
 Suicide.east <- Suicide[, list(sex, age, method, Freq.east=as.double(sample(1:1500, nrow(Suicide), replace=T)))]
@@ -52,7 +89,7 @@ setnames(Suicide, "Freq", "Freq.west")
 ## If you rerun the command above, but with the argument "all" set to true, do you get a different result? Why or why not?
 
 ## ------------------------------------------------------------------------------------
-## Section 5: Reshaping -----------------------------------------------------
+## EXTRA CREDIT 2: Reshaping -----------------------------------------------------
 
 ## Melt the "suicide.all" dataset so that the "Freq.west" and "Freq.east" columns are collapsed into a single column. 
 ## Save the result of this operation to a new dataset called "melted.suicide". 
@@ -60,11 +97,6 @@ setnames(Suicide, "Freq", "Freq.west")
 ## Cast the "melted.suicide" dataset so that there is a column for each sex, which contains the death count for that sex. 
 
 
-## ------------------------------------------------------------------------------------
-## Section 6: Contemplation -----------------------------------------------------
-## You've just been working with a dataset of suicides. We've been treating them like they're just 
-## any old numbers. Is this an appropriate method of working with this dataset? How do you, as a newfangled 
-## data scientist, feel about needing to work with depressing or sensitive information?
 
 
 
